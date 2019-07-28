8dad6aa599d0a4657a75534bc60bff7ab656b503
divyans7 <divyans7>
2019-07-28 12:24:43 +0530

    the last1

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-
+import YouTube from 'react-youtube'
 
 const VideoDetail = ({video}) => {
     
@@ -8,17 +8,28 @@ const VideoDetail = ({video}) => {
         return <div>Loading... </div>;
     
     } 
-    const videoID = video.id.videoID;
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+          autoplay: 1
+        }
+    };
+    const videoID = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoID}`;
 
-
+    console.log(videoID, video, "this is video id")
     return(
         <div className="video-detail col-md-8">
-            <div className="embed-responsive embed-responsive-16by9">
+            {/* <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responsive-item" src={url}></iframe>
 
-            </div>
-
+            </div> */}
+            <YouTube
+                videoId={videoID}
+                opts={opts}
+                // onReady={this._onReady}
+            />
 
             <div className="details">
                 <div>{video.snippet.title}</div>
